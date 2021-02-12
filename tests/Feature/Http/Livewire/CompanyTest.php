64ca4c4599370978bcf77c58306a3c57cc16d7b1
ipoint24 <reglindis.koch@ipoint24.de>
2021-02-12 22:43:05 +0100

    CompanyTest

--- tests/Feature/Http/Livewire/CompanyTest.php
+++ tests/Feature/Http/Livewire/CompanyTest.php
@@ -12,11 +12,35 @@ use Tests\TestCase;
 
 class CompanyTest extends TestCase
 {
+    use RefreshDatabase;
+
     /*
      * Guest / RegUser
      * Route
      * Validation
+     * CRUD
      */
+
+    public function test_guest_can_not_see_companies()
+    {
+        $response = $this->get('/companies');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_registered_user_can_see_companies()
+    {
+        $this->actingAs($user = User::factory()->create());
+        $response = $this->get('/companies');
+        $response->assertStatus(200);
+    }
+
+    function test_companies_page_contains_livewire_component()
+    {
+        $this->actingAs($user = User::factory()->create());
+        $this->get('/companies')->assertSeeLivewire('companies');
+    }
+
     public function test_company_name_is_required()
     {
         $this->actingAs($user = User::factory()->create());
@@ -61,5 +85,30 @@ class CompanyTest extends TestCase
             ->set('description', str_repeat("1", 201))
             ->call('store')
             ->assertHasErrors(['description' => 'max']);
+    }
+
+    public function test_user_can_create_or_update_a_company()
+    {
+        $this->actingAs($user = User::factory()->create());
+        Livewire::test(Companies::class)
+            ->set('title', 'Test Company')
+            ->set('description', 'Test description')
+            ->call('store');
+        $this->assertTrue(Company::whereTitle('Test Company')
+            ->whereDescription('Test description')
+            ->exists());
+    }
+
+    public function test_user_can_delete_a_company()
+    {
+        $this->actingAs($user = User::factory()->create());
+        $company = Company::factory()->create();
+        Livewire::test(Companies::class)
+            //->set('company_id', $company->id)
+            ->call('delete', $company->id);
+        $this->assertTrue(Company::whereId($company->id)
+            ->withTrashed()
+            ->whereNotNull($company->deleted_at)
+            ->exists());
     }
 }
